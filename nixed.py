ad08537ed25b75abdfdc92b5741e36d310f515e2
alyx <email@alxcore.org>
2023-06-15 03:11:22 -0500

    reorganizing
    [hopefully] increase readability

--- nixed.py
+++ nixed.py
@@ -2,23 +2,16 @@
 import os
 import argparse
 
-syscall = os.system
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--install', dest='install', action='store_true', help='Install queried package.')
 parser.add_argument('-sh', '--shell', dest='shell', action='store_true', help='Leverage `nix-shell` to test out queried package in a non-persistent shell environment')
-
 parser.add_argument('-r', '--remove', dest='remove', action='store_true', help='Remove queried package')
 parser.add_argument('-rl', '--remove-launcher', dest='removeLauncher', action='store_true', help='Manually delete launcher entry from ~/.local/share/applications/')
 parser.add_argument('-cd', '--collect-garbage', dest='garbageCollection', action='store_true', help='Clean up unused directory paths from old packages and shells')
-
 parser.add_argument('-u', '--update', dest='update', action='store_true', help='Update channels and installed packages.')
-
 parser.add_argument('-q', '--query', dest='query', action='store_true', help='Search store for queried package')
 parser.add_argument('-ls', '--list-installed', dest='listInstalled', action='store_true', help='Modification flag for --query. Specifies searching from installed packages rather than from store.')
-
 parser.add_argument('--debug', dest='debug', action='store_true', help='Manually edit launcher entry')
-
 parser.add_argument(dest='positionalArgument', action='append', nargs='?', help="Recommended to avoid requesting multiple packages in one command but if you do, put them quotes like so: nixed -i 'chromium firefox lynx'")
 
 args = parser.parse_args()
@@ -30,102 +23,101 @@ class positParser():
     noQuotes = noRbracket.replace("'", "")
     positOut = noQuotes
 
-class main():
-
-    class install():
-        def nur():
-            print('Nix user repo support is coming soon')
-        def main():
-            syscall('nix-env -iA %s' % (positParser.positOut))
-            install.createLauncher()
-        def createLauncher():
-            syscall('echo "[Desktop Entry]\nVersion=1.0\nType=Application\nName=%s\nComment=\nExec=%s\nIcon=%s\nPath=\nTerminal=false\nStartupNotify=false" >> ~/.local/share/applications/%s.desktop' % (positParser.positOut.capitalize(), positParser.positOut, positParser.positOut, positParser.positOut.capitalize()))
-            exit()
-        class shell():
-            def launch():
-                syscall('nix-shell')
-            def install():
-                if args.install == True:
-                    if positParser.positOut == 'None':
-                        print("Please select a package to test or drop the --install (`nixed --shell`) to invoke from shell.nix file")
-                        exit()
-                    else:
-                        syscall('nix-shell -p %s' % (positParser.positOut))
-    class remove():
-        def main():
-            syscall('nix-env -e %s' % (positParser.positOut))
-            remove.removeLauncher()
-            remove.garbageCollection()
-
-        def removeLauncher():
-            syscall('rm ~/.local/share/applications/%s.desktop' % (positParser.positOut.capitalize()))
-
-        def garbageCollection():
-            syscall('nix-collect-garbage')
-            syscall('nix-collect-garbage -d')
-    class update():
-        def main():
-            update.channelUpdate()
-            update.pkgUpdate()
-
-        def pkgUpdate():
-            syscall('nix-env -u')
-
-        def channelUpdate():
-            syscall('nix-channel --update')
-    class query():
-
-        def listInstalled():
-            if positParser.positOut == 'None':
-                syscall('nix-env -q --installed')
-            elif positParser.positOut != 'None':
-                syscall('nix-env -q %s --installed' % (positParser.positOut))
-        
-        def listAvailable():
-            syscall('nix-env -q %s --available' % (positParser.positOut))
-    class debug():
-        def editLauncher():
-            syscall('nano ~/.local/share/applications/%s.desktop' % (positParser.positOut.capitalize()))
-        def renameLauncher():
-            syscall('mv ~/.local/share/applications/%s.desktop ~/.local/share/applications/%s.desktop')
-
-if args.shell == True:
-    if args.install == True:
-        main.install.shell.install()
-        exit()
-    elif args.install != True:
-        main.install.shell.launch()
+class install():
+    def nur():
+        print('Nix user repo support is coming soon')
+    def main():
+        os.system('nix-env -iA %s' % (positParser.positOut))
+        install.createLauncher()
+    def createLauncher():
+        os.system('echo "[Desktop Entry]\nVersion=1.0\nType=Application\nName=%s\nComment=\nExec=%s\nIcon=%s\nPath=\nTerminal=false\nStartupNotify=false" >> ~/.local/share/applications/%s.desktop' % (positParser.positOut.capitalize(), positParser.positOut, positParser.positOut, positParser.positOut.capitalize()))
         exit()
 if args.install == True:
     if args.shell != True:
-        main.install.main()
+        install.main()
         exit()
+class shell():
+    def launch():
+        os.system('nix-shell')
+    def install():
+        if args.install == True:
+            if positParser.positOut == 'None':
+                print("Please select a package to test or drop the --install (`nixed --shell`) to invoke from shell.nix file")
+                exit()
+            else:
+                os.system('nix-shell -p %s' % (positParser.positOut))
+if args.shell == True:
+    if args.install == True:
+        shell.install()
+        exit()
+    elif args.install != True:
+        shell.launch()
+        exit()
+class remove():
+    def main():
+        os.system('nix-env -e %s' % (positParser.positOut))
+        remove.removeLauncher()
+        remove.garbageCollection()
+    def removeLauncher():
+        os.system('rm ~/.local/share/applications/%s.desktop' % (positParser.positOut.capitalize()))
+    def garbageCollection():
+        os.system('nix-collect-garbage')
+        os.system('nix-collect-garbage -d')
 if args.remove == True:
     if args.install != True:
-        main.remove.main()
+        remove.main()
         exit()
-if args.update == True:
-    main.update.main()
-    exit()
 if args.garbageCollection == True:
-    main.remove.garbageCollection()
+    if args.remove != True:
+        remove.garbageCollection()
+        exit()
+if args.removeLauncher == True:
+    if args.remove != True:
+        remove.removeLauncher()
+        exit()
+class update():
+    def main():
+        update.channelUpdate()
+        update.pkgUpdate()
+    def pkgUpdate():
+        os.system('nix-env -u')
+    def channelUpdate():
+        os.system('nix-channel --update')
+if args.update == True:
+    update.main()
     exit()
+class query():
+    def listInstalled():
+        if positParser.positOut == 'None':
+            os.system('nix-env -q --installed')
+        elif positParser.positOut != 'None':
+            os.system('nix-env -q %s --installed' % (positParser.positOut))
+    def listAvailable():
+        os.system('nix-env -q %s --available' % (positParser.positOut))
+if args.query == True:
+    if args.listInstalled == True:
+        query.listInstalled()
+        exit()
+    else:
+        query.listAvailable()
+        exit()
+else:
+    print("Unknown command '%s'\nType 'nixed -h' to view available options" % (positParser.positOut))
+class debug():
+    def editLauncher():
+        os.system('nano ~/.local/share/applications/%s.desktop' % (positParser.positOut.capitalize()))
+    def renameLauncher():
+        os.system('mv ~/.local/share/applications/%s.desktop ~/.local/share/applications/%s.desktop')
 if args.debug == True:
     if positParser.positOut == 'None':
         print('Please specify a package launcher you wish to correct')
         exit()
     if positParser.positOut != 'None':
-        main.debug.editLauncher()
+        debug.editLauncher()
         exit()
-if args.removeLauncher == True:
-    main.remove.removeLauncher()
-    exit()
-if args.query == True:
-    if args.listInstalled == True:
-        main.query.listInstalled()
-        exit()
-    else:
-        main.query.listAvailable()
-        exit()
-else:
-    print("Unknown command '%s'\nType 'nixed -h' to view available options" % (positParser.positOut))
+
+
+
+
+
+
